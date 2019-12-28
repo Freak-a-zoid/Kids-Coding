80c7b759646f31519a604cc1ebc2d94361d17df7
Freakazoid <37374995+Freak-a-zoid@users.noreply.github.com>
2019-12-28 08:44:09 +0200

    Create circle example.

---
+++ 1. Introduction to shapes/1.1 Drawing a circle/Example.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  // Add code to create shapes here.
+  // Circle call contains 4 parameters:
+  // x coordinate/position.
+  // y coordinate/position.
+  // width
+  // height
+  circle( 200, 200, 75, 75 );
+}
