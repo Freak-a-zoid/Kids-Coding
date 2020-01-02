ffb7145cd1c4c337ac6c15e0f3506bf9b5b43cfd
Waldo van Niekerk <waldovanniekerk147@gmail.com>
2020-01-02 19:28:03 +0200

    Updated Introduction ReadMe.md. Added links to newly created sub sections.

--- 1. Introduction to shapes/Introduction-to-shapes-nav.js
+++ 1. Introduction to shapes/Introduction-to-shapes-nav.js
@@ -13,6 +13,12 @@ document.write(
         <li> \
           <a href="/Kids-Coding/1.%20Introduction%20to%20shapes/Drawing%20shapes/1.3%20Drawing%20a%20rectangle">Drawing a rectangle.</a> \
         </li> \
+        <li> \
+          <a href="/Kids-Coding/1.%20Introduction%20to%20shapes/Drawing%20shapes/1.4%20Drawing%20a%20triangle">Drawing a triangle.</a> \
+        </li> \
+        <li> \
+          <a href="/Kids-Coding/1.%20Introduction%20to%20shapes/Drawing%20shapes/1.5%20Drawing%20multiple%20shapes">Drawing multiple shapes.</a> \
+        </li> \
       </ol> \
   </ol> \
 ');
